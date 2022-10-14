2b06ab0bfce226d1feee8ebec47c28fceee949f8
TabithaKoech <tabbyk1718@gmail.com>
2022-10-14 07:36:42 +0300

    created a concert with user  seraializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
-# class UserSerializer < ActiveModel::Serializer
-#   attributes :id, :username, :password, :is_admin
-# end
+class UserSerializer < ActiveModel::Serializer
+  attributes :id, :username, :password, :is_admin
+end
