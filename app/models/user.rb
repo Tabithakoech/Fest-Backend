07af727b9863e816cb1546ba2e8277eba0c82f76
TabithaKoech <tabbyk1718@gmail.com>
2022-10-14 07:22:52 +0300

    created a users model and added its associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-# class User < ApplicationRecord
-#     has_many :comments
-#     has_many :concerts, through: :comments
+class User < ApplicationRecord
+    has_many :comments
+    has_many :concerts, through: :comments
 
-#     has_secure_password
-#     validates :username, presence: true, uniqueness: true
-#     validates :password, presence: true, length: { minimum: 6 }
-# end
+    has_secure_password
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true, length: { minimum: 6 }
+end
