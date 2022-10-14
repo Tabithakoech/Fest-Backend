9e454501177ccca6b0c780795d33b340a5c8cc1b
TabithaKoech <tabbyk1718@gmail.com>
2022-10-14 07:35:03 +0300

    created a concert seraializer  and added the index and show routes

--- app/serializers/concert_serializer.rb
+++ app/serializers/concert_serializer.rb
@@ -1,3 +1,3 @@
-# class ConcertSerializer < ActiveModel::Serializer
-#   attributes :id, :name, :band_name, :genre, :date, :time, :image
-# end
+class ConcertSerializer < ActiveModel::Serializer
+  attributes :id, :name, :band_name, :genre, :date, :time, :image
+end
