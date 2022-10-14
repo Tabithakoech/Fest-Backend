869cd07d9c5f9543b3db14652b9fa5d8bc422fb3
TabithaKoech <tabbyk1718@gmail.com>
2022-10-14 07:28:47 +0300

    craeted comments controller and added get and post routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,54 +1,54 @@
-# class CommentsController < ApplicationController
-#     # before_action :authenticate_user, except: [:show, :index]
-#     before_action :comment_find, only: [:show, :edit, :update, :destroy]
-#     before_action :authorize_comment, only: [:update, :edit, :destroy]
+class CommentsController < ApplicationController
+    # before_action :authenticate_user, except: [:show, :index]
+    before_action :comment_find, only: [:show, :edit, :update, :destroy]
+    before_action :authorize_comment, only: [:update, :edit, :destroy]
 
-#     def index
-#         render json: Comment.all
-#     end
+    def index
+        render json: Comment.all
+    end
 
-#     def create
-#         # byebug
-#         @comment = @current_user.comments.create!(comment_params)
-#         render json: comments, status: :created
-#     end
+    def create
+        # byebug
+        @comment = @current_user.comments.create!(comment_params)
+        render json: comments, status: :created
+    end
 
-#     def show
-#         #comment = comment_find
-#         render json: @comment
-#     end
+    def show
+        #comment = comment_find
+        render json: @comment
+    end
 
-#     def update
-#         #comment = comment_find
-#         @comment.update!(comment_params)
-#         render json: @comment
-#     end
+    def update
+        #comment = comment_find
+        @comment.update!(comment_params)
+        render json: @comment
+    end
 
-#     def destroy 
-#         #comment = @current_user.comment_find
-#         @comment.destroy
-#     end
+    def destroy 
+        #comment = @current_user.comment_find
+        @comment.destroy
+    end
 
-#     private
+    private
 
-#     def comment_find
-#         @comment = Comment.find(params[:id])
-#     end
+    def comment_find
+        @comment = Comment.find(params[:id])
+    end
 
-#     def comment_params
-#         params.permit(:content, :rating, :concert_id, :user_id)
-#     end
+    def comment_params
+        params.permit(:content, :rating, :concert_id, :user_id)
+    end
 
-#     # def correct_user
-#     #     comment = Comment.find_by(id: params[:id])
-#     #     unless current_user?(comment.user)
-#     #       redirect_to user_path(current_user)
-#     # end
+    # def correct_user
+    #     comment = Comment.find_by(id: params[:id])
+    #     unless current_user?(comment.user)
+    #       redirect_to user_path(current_user)
+    # end
 
-#     def authorize_comment
-#         unless @current_user == @comment.user 
-#           redirect_to "/comments"
-#         end
-#       end
+    def authorize_comment
+        unless @current_user == @comment.user 
+          redirect_to "/comments"
+        end
+      end
 
-# end
+end
